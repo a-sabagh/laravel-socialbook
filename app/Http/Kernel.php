c934d13df55b27b176e912d8df5c50280a9cb0d5
asabagh <a.sabagh72@gmail.com>
2018-12-26 20:34:06 +0330

    change route api for post put and delte api book

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Http\Middleware\RefreshToken::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
